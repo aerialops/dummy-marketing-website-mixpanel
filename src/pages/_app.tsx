93018ba773420e2ab2b16b9970dd10f9b19558d3
nicolassanmar <nicolassanmar@gmail.com>
2024-02-14 16:46:50 -0300

    add mixpanel tracking

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,47 @@ import { type AppType } from "next/dist/shared/lib/utils";
 import Head from "next/head";
 import { Exo } from "next/font/google";
 import "~/styles/globals.css";
+import React from "react";
+
+import mixpanel from "mixpanel-browser";
+import { env } from "~/env";
+import { useRouter } from "next/router";
 
 const exoFont = Exo({ subsets: ["latin"] });
 
+const useInitMixpanel = () => {
+  React.useEffect(() => {
+    mixpanel.init(env.NEXT_PUBLIC_MIXPANEL_PROJECT_KEY, {
+      debug: true,
+      track_pageview: false,
+    });
+  }, []);
+};
+
+const useTrackPageView = () => {
+  const router = useRouter();
+
+  React.useEffect(() => {
+    const trackPageChange = (to: string) =>
+      mixpanel.track_pageview({
+        from: router.asPath,
+        to,
+      });
+
+    router.events.on("hashChangeStart", trackPageChange);
+    router.events.on("routeChangeStart", trackPageChange);
+
+    return () => {
+      router.events.off("hashChangeStart", trackPageChange);
+      router.events.off("routeChangeStart", trackPageChange);
+    };
+  }, [router.asPath, router.events]);
+};
+
 const MyApp: AppType = ({ Component, pageProps }) => {
+  useInitMixpanel();
+  useTrackPageView();
+
   return (
     <>
       <Head>
