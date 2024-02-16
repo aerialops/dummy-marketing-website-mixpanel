cca4c2a7d0ef7c083834df429425d042d2dc7aa4
nicolassanmar <nicolassanmar@gmail.com>
2024-02-16 14:35:53 -0300

    Feat: Add pricing cards with links to external website

--- src/pages/pricing.tsx
+++ src/pages/pricing.tsx
@@ -1,18 +1,55 @@
 import { Background } from "~/components/background";
 import { Header } from "~/components/header";
 
+const PricingCard: React.FC<{
+  title: string;
+  description: string;
+  price: string;
+}> = ({ title, description, price }) => {
+  return (
+    <div className="flex flex-grow flex-col rounded-lg bg-white p-6 shadow-lg">
+      <h3 className="mb-4 text-2xl font-bold text-blue-700">{title}</h3>
+
+      <div className="mb-4">
+        <p>{description}</p>
+      </div>
+
+      <div className="flex-grow" />
+      <h2 className="mb-4 text-3xl font-bold">{price}</h2>
+
+      <a
+        href="https://calendly.com/doug-aerial/website-demo-request"
+        target="_blank"
+        className="rounded-lg bg-blue-500 px-4 py-2 text-white"
+      >
+        Schedule a demo
+      </a>
+    </div>
+  );
+};
+
 export default function Page() {
   return (
     <Background>
       <Header />
       <header className="flex flex-grow flex-col items-center justify-center space-y-4 px-8 py-16 text-center">
-        <h1 className="text-6xl font-bold leading-tight">Dummy text</h1>
-        <p className="text-xl text-gray-600">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex placeat
-          dolorum impedit molestiae eligendi, minima earum! Error recusandae
-          perspiciatis voluptas eum! Officiis natus totam maiores optio aut
-          alias consequuntur aliquam!
-        </p>
+        <div className="grid max-w-4xl grid-cols-3 gap-4">
+          <PricingCard
+            title="Launch"
+            description="Early stage founders with less than $2M raised can get started using Aerial for free."
+            price="Free!"
+          />
+          <PricingCard
+            title="Grow"
+            description="Congratulations! You have found a problem, a solution, and now you need to grow. Our growth plan offers expanded feature sets all at a fraction of your lawyers hourly rate."
+            price="$250/month"
+          />
+          <PricingCard
+            title="Scale"
+            description="It’s time to expand, and we have your back with custom built solutions for your company."
+            price="$495/month"
+          />
+        </div>
       </header>
     </Background>
   );
