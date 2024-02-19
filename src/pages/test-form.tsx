071a59a41391e1deced8fec6ac1f9f3d2e55059c
nicolassanmar <nicolassanmar@gmail.com>
2024-02-19 13:27:31 -0300

    Feat: Track forms that redirect

--- src/pages/test-form.tsx
+++ src/pages/test-form.tsx
@@ -1,20 +1,11 @@
 import mixpanel from "mixpanel-browser";
+import React from "react";
 import { Background } from "~/components/background";
 import { Header } from "~/components/header";
 
-const TestForm: React.FC = () => {
+const FormInputs: React.FC = () => {
   return (
-    <form
-      className="  flex max-w-2xl flex-grow flex-col rounded-lg bg-white p-6 shadow-lg"
-      onSubmit={(event) => {
-        event.preventDefault();
-        const formData = new FormData(event.currentTarget);
-        const formEntries = Object.fromEntries(formData.entries());
-        mixpanel.track("Form submitted", {
-          formEntries,
-        });
-      }}
-    >
+    <>
       <div className="mb-4 flex flex-col">
         <label htmlFor="name" className="mb-2">
           Name
@@ -54,6 +45,54 @@ const TestForm: React.FC = () => {
       >
         Submit
       </button>
+    </>
+  );
+};
+
+const FormWithoutRedirect: React.FC = () => {
+  return (
+    <form
+      className="flex max-w-2xl flex-grow flex-col rounded-lg bg-white p-6 shadow-lg"
+      onSubmit={(event) => {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        const formEntries = Object.fromEntries(formData.entries());
+        mixpanel.track("Form submitted", {
+          formEntries,
+          formId: "invite-form-without-redirect",
+        });
+      }}
+    >
+      <FormInputs />
+    </form>
+  );
+};
+
+const FormWithRedirect: React.FC = () => {
+  const ref = React.useRef<HTMLFormElement>(null);
+  // Has to be tracked in an effect, as the element must be in the DOM before Mixpanel can attach the event listener.
+  // As a workaround, this has to be wrapped in a `setTimeout` as well. See issue here:
+  // https://github.com/mixpanel/mixpanel-js/issues/419#issue-2142437047
+  React.useEffect(() => {
+    setTimeout(() => {
+      if (ref.current === null) return;
+      // All form submissions should be tracked with different event names, as they are different actions.
+      mixpanel.track_forms(ref.current, "Form submitted", () => {
+        if (ref.current === null) return;
+        // We use `ref.current` instead of the argument passed to the callback, as the argument is not typed.
+        const formData = new FormData(ref.current);
+        const formEntries = Object.fromEntries(formData.entries());
+        return { formEntries, formId: "invite-form-with-redirect" };
+      });
+    }, 0);
+  }, []);
+
+  return (
+    <form
+      className="flex max-w-2xl flex-grow flex-col rounded-lg bg-white p-6 shadow-lg"
+      ref={ref}
+    >
+      <FormInputs />
     </form>
   );
 };
@@ -62,16 +101,26 @@ export default function Page() {
   return (
     <Background>
       <Header />
-      <header className="flex flex-grow flex-col items-center justify-center space-y-4 px-8 py-16 text-center">
-        <h1 className="text-6xl font-bold leading-tight">Test form</h1>
-      </header>
-      <div className="flex flex-grow items-center justify-center">
-        <TestForm />
-      </div>
-      <div>
-        <p className="mb-80 text-center text-white">
-          This form is not connected to anything.
-        </p>
+      <div className="flex flex-grow flex-row">
+        <div className="flex flex-grow flex-col items-center justify-center space-y-4 px-8 py-16 text-center">
+          <h1 className="text-6xl font-bold leading-tight">No redirect</h1>
+          <FormWithoutRedirect />
+          <div>
+            <p className="mb-80 text-center text-white">
+              This form does not redirect on submit.
+            </p>
+          </div>
+        </div>
+
+        <div className="flex flex-grow flex-col items-center justify-center space-y-4 px-8 py-16 text-center">
+          <h1 className="text-6xl font-bold leading-tight">Redirect</h1>
+          <FormWithRedirect />
+          <div>
+            <p className="mb-80 text-center text-white">
+              This form redirects on submit.
+            </p>
+          </div>
+        </div>
       </div>
     </Background>
   );
